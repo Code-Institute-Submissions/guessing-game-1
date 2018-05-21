eb2a24134b14cd552eac33fd329b80d9240815f0
Deniss Matvejevs <mden7785@gmail.com>
2018-05-21 15:16:15 +0000

    Upload quize game built on Python

--- run.py
+++ run.py
@@ -1,44 +1,56 @@
-import os
-import json
-import random
+import random, json, os
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 
-def randomQuestionsList(data_len, data):
-    """
-    Generate list of first 5 random numbers of data length
-    """
-    random_list = range(data_len)
-    random.shuffle(random_list)
+
+def quiz_answer(data):
     
-    list = []
+    count = 0
     
-    for number in random_list[:5]:
-        list.append(data[number])
+    for i in range(len(request.form)):
+        answers = request.form[str(i+1)]
+        for j in range(len(data)):
+            if answers == data[j]['answer']:
+                count += 1
+    return count
+
+"""
+Function if_user_exist() check username.json data for filled username
+If username exist function return true
+"""    
+def if_user_exist(user_data, username):
+    for item in user_data:
+        if username in item:
+            return True
         
-    return list
-    
-    
-def getAnswers(data):
-    formData = request.form
-    
-    list = []
-    
-    randomData = randomQuestionsList(len(data), data)
-    for val in formData.values():
-        for i in range(0, 5):
-            if val == randomData[i].get('answer'):
-                list.append(val)
-        
-    return list            
+"""
+Function user_to_json() create new username data field 
+If if_user_exist() return true
+"""   
+def user_to_json(user_data, username):
+    if not if_user_exist(user_data, username):
+        with open('data/username.json', 'w') as user:
+            user_dict = {username:{'name':username}}
+            user_data.append(user_dict)
+            json.dump(user_data, user, indent=2)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
     if request.method == "POST":
-        return redirect(request.form['username'])
+        
+        username = request.form['username']
+        
+        if not username: 
+            flash('Please fill out username field')
+            return redirect(url_for('index'))
+        else:
+            with open('data/username.json', 'r') as user_data:
+                user_data = json.load(user_data)
+                user_to_json(user_data, username)
+            return redirect(request.form['username'])
         
     return render_template('index.html')
     
@@ -47,11 +59,13 @@ def index():
 def questions(username):
     with open('data/qa.json', 'r') as json_data:
         data = json.load(json_data)
-            
-    if request.method == "POST":
-        flash(getAnswers(data))
+        random.shuffle(data)
         
-    return render_template('questions.html', questions=randomQuestionsList(len(data), data), username = username)
+    if request.method == "POST":
+        flash('Correct answers {} out of 5'.format(quiz_answer(data)))
+        return redirect(url_for('result'))
+        
+    return render_template('questions.html', data = data[:5], username = username)
 
 
 @app.route('/result')
---
+++ test_run.py
@@ -0,0 +1,8 @@
+import unittest
+import run
+from string import ascii_uppercase
+
+
+
+if __name__ == '__main__':
+    unittest.main()
