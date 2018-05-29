c1cf9f81f835f515004feba071892f775da35475
Deniss Matvejevs <mden7785@gmail.com>
2018-05-29 12:38:30 +0000

    Add user friendly input and update questions preview for each question own page. In addition implement unittest

--- run.py
+++ run.py
@@ -1,18 +1,62 @@
-import random, json, os
+import random, json, os, re
 from flask import Flask, render_template, request, redirect, url_for, flash
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 
+step = 0
+count = 0
 
-def quiz_answer(data):
-    count = 0
+"""
+Remove articles from answer
+"""  
+def remove_articles(text):
+    nltk.download('stopwords')
+    nltk.download('punkt')
     
-    for i in range(len(request.form)):
-        answers = request.form[str(i+1)]
-        for j in range(len(data)):
-            if answers.lower() == data[j]['answer'].lower():
-                count += 1
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    
+    filtered_sentence = []
+    
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+            
+    myList = ' '.join(map(str, filtered_sentence)) 
+    
+    myList = re.sub(" \,", ",", myList)
+    myList = re.sub(" \.", ".", myList)
+    
+    return myList
+
+"""
+Return correct answers per each question
+"""  
+def quiz_answer(data):
+    global count
+    for i in range(len(data)):
+        
+        answers = request.form['1'].lower()
+        correct_answer = data[i]['answer'].lower()
+        
+        if len(answers) != len(correct_answer):
+            
+            answers = remove_articles(answers)
+            
+            if not answers:
+                count += 0 
+            elif not answers.strip():
+                count += 0 
+            elif answers in correct_answer:
+                count += .5
+        
+        elif answers == correct_answer:
+            count += 1
+                
     return count
 
 """
@@ -43,7 +87,7 @@ Function receive scors and append to data
 def append_user_result_in_data(username, score):
     with open('data/username.json', 'r') as users_data:
         users_data = json.load(users_data)
-        
+    
     for item in users_data:
         if username.lower() in item['user']['name']:
             item['user'].update({'score': score}) 
@@ -58,6 +102,10 @@ def index():
     if request.method == "POST":
         
         username = request.form['username']
+        global step 
+        global count 
+        step = 0
+        count = 0
         
         if not username: 
             flash('Please fill out username field')
@@ -66,41 +114,47 @@ def index():
             with open('data/username.json', 'r') as user_data:
                 user_data = json.load(user_data)
                 user_to_json(user_data, username)
+            
+            with open('data/qa.json', 'r') as json_data:
+                data = json.load(json_data)
+                random.shuffle(data)
+                five_random_q = data[:5]
+                
+            with open('data/five_questions.json', 'w') as five_q:
+                json.dump(five_random_q, five_q, indent=2)
+            
             return redirect(request.form['username'])
         
     return render_template('index.html')
     
 
+
 @app.route('/<username>', methods=["GET", "POST"])
 def questions(username):
-    with open('data/qa.json', 'r') as json_data:
+    with open('data/five_questions.json', 'r') as json_data:
         data = json.load(json_data)
-        random.shuffle(data)
-        
+    #flash(data[0]['answer'])
     if request.method == "POST":
-        flash(username)
-        flash('Correct answers {} out of 5'.format(quiz_answer(data)))
-
         append_user_result_in_data(username, quiz_answer(data))
-
-        return redirect(url_for('result', username = username))
+        global step 
+        if step < 4:
+            step += 1
+            return render_template('questions.html', username = username, data = data[step])
+        else:
+            return redirect(url_for('result', username = username, score = quiz_answer(data)))
         
-    return render_template('questions.html', data = data[:5], username = username)
+    return render_template('questions.html', data = data[0], username = username)
+
 
 
 @app.route('/result')
 def result():
-    return render_template('result.html')
-    
-
-@app.route('/chartlist')
-def chartlist():
     with open('data/username.json', 'r') as data:
         data = json.load(data)
         #newlist = sorted(data, key=itemgetter('score'), reverse=True)
         data.sort(key=lambda e: e['user']['score'], reverse=True)
         
-    return render_template('chartlist.html', data = data)
+    return render_template('result.html', data = data, username= request.args.get('username'), score = request.args.get('score'))
         
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
--- test_run.py
+++ test_run.py
@@ -1,8 +1,24 @@
 import unittest
 import run
+import json
 from string import ascii_uppercase
 
-
+class TestRun(unittest.TestCase):
+    
+    def test_remove_articles_from_string(self):
+        some_articles = "Please give me some water."
+        an_article = "Please give me an ice cube."
+        
+        self.assertNotEqual(run.remove_articles('the'), 'the')
+        self.assertNotEqual(run.remove_articles(some_articles), some_articles)
+        self.assertNotEqual(run.remove_articles(an_article), an_article)
+        
+    def test_if_user_exist(self):
+        with open('data/username.json', 'r') as user_data:
+            user_data = json.load(user_data)
+        
+        self.assertTrue(run.if_user_exist(user_data, 'denis'))
+        self.assertFalse(run.if_user_exist(user_data, 'Ben'))
 
 if __name__ == '__main__':
     unittest.main()
