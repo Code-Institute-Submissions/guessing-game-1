dfa21cc14aa799a038522410904dbd97df41e88a
Deniss Matvejevs <mden7785@gmail.com>
2018-08-05 19:11:29 +0000

    Updated style

--- test_run.py
+++ test_run.py
@@ -1,5 +1,5 @@
 import unittest
-import run
+from utils import user_exist, remove_articles
 import json
 from string import ascii_uppercase
 
@@ -9,16 +9,16 @@ class TestRun(unittest.TestCase):
         some_articles = "Please give me some water."
         an_article = "Please give me an ice cube."
         
-        self.assertNotEqual(run.remove_articles('the'), 'the')
-        self.assertNotEqual(run.remove_articles(some_articles), some_articles)
-        self.assertNotEqual(run.remove_articles(an_article), an_article)
+        self.assertNotEqual(remove_articles('the'), 'the')
+        self.assertNotEqual(remove_articles(some_articles), some_articles)
+        self.assertNotEqual(remove_articles(an_article), an_article)
         
     def test_if_user_exist(self):
         with open('data/username.json', 'r') as user_data:
             user_data = json.load(user_data)
         
-        self.assertTrue(run.if_user_exist(user_data, 'denis'))
-        self.assertFalse(run.if_user_exist(user_data, 'Ben'))
+        self.assertTrue(user_exist(user_data, 'denis'))
+        self.assertFalse(user_exist(user_data, 'Ben'))
 
 if __name__ == '__main__':
     unittest.main()
